528d451e060f148f71f97ceebe117769bc3bb618
Oswis-pai-02 <oswaldo.do-ch@hotmail.com>
2024-02-07 20:10:30 -0600

    Login completo con updates

--- src/routes/user.js
+++ src/routes/user.js
@@ -32,9 +32,9 @@ router.get("/users/:id", (req, res) => {
 //update a user
 router.put("/users/:id", (req, res) => {
     const { id } = req.params;
-    const {name, age, email } = req.body;
+    const {rol, nombreCompleto, correo, contrasenia, fechaDeNacimiento, genero } = req.body;
     userSchema
-        .updateOne({ _id: id }, { $set: {name, age, email}})
+        .updateOne({ _id: id }, { $set: {rol, nombreCompleto, correo, contrasenia, fechaDeNacimiento, genero}})
         .then((data)=> res.json(data))
         .catch((error) => res.json({message: error}));
 });
@@ -47,4 +47,24 @@ router.delete("/users/:id", (req, res) => {
         .then((data)=> res.json(data))
         .catch((error) => res.json({message: error}));
 });
+
+
+// Ruta para obtener la contraseña por correo electrónico
+router.get('/recover-password/:correo', (req, res) => {
+    const { correo } = req.params;
+    userSchema
+        .findOne({ correo: correo }) // Usar findOne y pasar el objeto de consulta correcto
+        .select('contrasenia -_id') // Solo seleccionar la contraseña y excluir el _id
+        .then((user) => {
+            if (user) {
+                // Devolver solo la contraseña, aunque esto sigue siendo inseguro
+                res.json({ contrasenia: user.contrasenia });
+            } else {
+                // Manejar el caso en que no se encuentre el usuario
+                res.status(404).json({ message: 'Usuario no encontrado' });
+            }
+        })
+        .catch((error) => res.status(500).json({ message: error.message }));
+});
+
 module.exports = router;
