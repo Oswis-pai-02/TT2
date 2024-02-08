323535332a7116dd3204c973d0a5a9639434cdb7
Admin Montoya <adjamoro@gmail.com>
2024-02-08 15:48:05 -0600

    Se actualiza el metodo create user y get Password

--- src/routes/user.js
+++ src/routes/user.js
@@ -8,8 +8,11 @@ router.post("/users", (req, res) => {
     const user = userSchema(req.body);
     user
         .save()
-        .then((data)=> res.json(data))
-        .catch((error) => res.json({message: error}));
+        .then((savedUser) => res.json({ 
+            message: "Usuario registrado correctamente",
+            userId: savedUser._id // Devuelve el ID del usuario creado
+        }))
+        .catch((error) => res.json({ message: error.message }));
 });
 
 //get all users
@@ -50,15 +53,15 @@ router.delete("/users/:id", (req, res) => {
 
 
 // Ruta para obtener la contraseña por correo electrónico
-router.get('/recover-password/:correo', (req, res) => {
+router.get('/users/getPassword/:correo', (req, res) => {
     const { correo } = req.params;
     userSchema
         .findOne({ correo: correo }) // Usar findOne y pasar el objeto de consulta correcto
-        .select('contrasenia -_id') // Solo seleccionar la contraseña y excluir el _id
+        .select('contrasenia') // Solo seleccionar la contraseña, _id se incluye por defecto
         .then((user) => {
             if (user) {
-                // Devolver solo la contraseña, aunque esto sigue siendo inseguro
-                res.json({ contrasenia: user.contrasenia });
+                // Devolver el ID y la contraseña
+                res.json({ id: user._id, contrasenia: user.contrasenia });
             } else {
                 // Manejar el caso en que no se encuentre el usuario
                 res.status(404).json({ message: 'Usuario no encontrado' });
