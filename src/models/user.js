3ff9914bb3ec144360003b270c1d75e112f37db3
Oswis-pai-02 <oswaldo.do-ch@hotmail.com>
2024-02-07 18:22:54 -0600

    ajustes

--- src/models/user.js
+++ src/models/user.js
@@ -1,18 +1,31 @@
 const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: {
+    rol: {
         type: String,
         required: true
     },
-    age: {
-        type: Number,
+    nombreCompleto: {
+        type: String,
         required: true
     },
-    email: {
+    correo: {
+        type: String,
+        required: true
+    },
+    contrasenia: {
+        type: String,
+        required: true
+    },
+    fechaDeNacimiento: {
+        type: String,
+        required: true
+    },
+    genero: {
         type: String,
         required: true
     }
 });
+
 
 module.exports = mongoose.model('User', userSchema);
