bf5c8595a89babbcc52a4934409763e5b8b4010d
Oswis-pai-02 <oswaldo.do-ch@hotmail.com>
2024-02-14 09:45:30 -0600

    Report actualizado al esquema

--- src/models/report.js
+++ src/models/report.js
@@ -1,20 +1,62 @@
 const mongoose = require('mongoose');
 
 const reportSchema = new mongoose.Schema({
-    //id_usuario
-    descripcion: { type: String, required: true },
-    //fechaHora
-    //titulo
-    //autor
-    //estacion
-    //linea
-    //direccion
-    imagen: { type: Buffer, required: true }, 
-    contentType: { type: String, required: true },  
-    //listaDeUsuariosQueDieronLike
-    likes: { type: Number, default: 0 }, //lista de identificadores de usuarios
-    //listaDeUsuariosQueDieronDislike
-    dislikes: { type: Number, default: 0 } //lista de identificadores de usuarios
+    descripcion: {
+        type: String,
+        required: true
+    },
+    imagen: {
+        type: Buffer,
+        required: true
+    },
+    contentType: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    dislikes: {
+        type: Number,
+        default: 0
+    },
+    id_usuario: {
+        type: mongoose.Schema.Types.ObjectId, // Suponiendo que es una referencia a un usuario
+        required: true
+    },
+    fechaHora: {
+        type: Date, // Suponiendo que quieres almacenar tanto la fecha como la hora
+        required: true
+    },
+    titulo: {
+        type: String,
+        required: true
+    },
+    autor: {
+        type: String, // Suponiendo que 'autor' es un nombre o identificador en texto
+        required: true
+    },
+    estacion: {
+        type: String, // Suponiendo que 'estacion' se refiere a una estación de metro o similar
+        required: true
+    },
+    linea: {
+        type: String, // Suponiendo que 'linea' se refiere a la línea de un metro o autobús
+        required: true
+    },
+    direccion: {
+        type: String, // Suponiendo que 'direccion' se refiere a una dirección o un sentido
+        required: true
+    },
+    listaDeUsuariosQueDieronLike: [{
+        type: mongoose.Schema.Types.ObjectId, // Suponiendo que es una lista de identificadores de usuarios
+        ref: 'Usuario' // Asumiendo que tienes un modelo 'Usuario'
+    }],
+    listaDeUsuariosQueDieronDislike: [{
+        type: mongoose.Schema.Types.ObjectId, // Suponiendo que es una lista de identificadores de usuarios
+        ref: 'Usuario' // Asumiendo que tienes un modelo 'Usuario'
+    }]
 });
 
 module.exports = mongoose.model('reportes', reportSchema);
