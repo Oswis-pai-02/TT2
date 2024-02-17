8ba91f6ea5afba54e428f399b825b25aa59fb691
Admin Montoya <adjamoro@gmail.com>
2024-02-16 21:40:03 -0600

    Crear reporte completo de manera exitosa

--- src/routes/report.js
+++ src/routes/report.js
@@ -1,28 +1,47 @@
 const express = require('express');
 const multer = require('multer');
 const router = express.Router();
-const Report = require('../models/report'); // Asegúrate de reemplazar esto con tu path correcto
+const Report = require('../models/report');
 
 // Configuración de Multer para manejar la carga de archivos en memoria
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-router.post('/report', upload.single('imagen'), (req, res) => {
+router.post('/report', upload.single('imagen'), async (req, res) => {
+    try {
+        let imagenBuffer = null;
+        let contentType = null;
 
-    if (!req.file) {
-        return res.status(400).send('No se encontró archivo de imagen.');
+        // Verificar si se proporcionó una imagen en la solicitud
+        if (req.file) {
+            imagenBuffer = req.file.buffer;
+            contentType = req.file.mimetype;
+        }
+
+        // Calcular la fecha y hora actual
+        const fechaHoraActual = new Date();
+
+        // Crear un nuevo reporte con la información proporcionada en la solicitud
+        const newReport = new Report({
+            descripcion: req.body.descripcion,
+            imagen: imagenBuffer,
+            contentType: contentType,
+            id_usuario: req.body.id_usuario,
+            titulo: req.body.titulo,
+            autor: req.body.autor,
+            estacion: req.body.estacion,
+            linea: req.body.linea,
+            direccion: req.body.direccion,
+            fechaHora: fechaHoraActual
+        });
+
+        // Guardar el nuevo reporte en la base de datos
+        await newReport.save();
+
+        res.json("Reporte creado de forma exitosa");
+    } catch (error) {
+        res.status(500).json({ error: error.message });
     }
-
-    // Crear un nuevo reporte con la imagen recibida
-    const newReport = new Report({
-        descripcion: req.body.descripcion,
-        imagen: req.file.buffer,  // Imagen como Buffer
-        contentType: req.file.mimetype  // Tipo MIME de la imagen
-    });
-
-    newReport.save()
-        .then(report => res.json("Reporte creado de forma exitosa"))
-        .catch(err => res.json({ error: err.message }));
 });
 
 module.exports = router;
