7dcf1c88dfdca13889206de8f6a6eb481d5507c7
Oswis-pai-02 <oswaldo.do-ch@hotmail.com>
2024-02-18 17:12:27 -0600

    implementacion like y dislike

--- src/routes/report.js
+++ src/routes/report.js
@@ -25,4 +25,28 @@ router.post('/report', upload.single('imagen'), (req, res) => {
         .catch(err => res.json({ error: err.message }));
 });
 
+// Ruta para agregar un like a un reporte
+router.post('/reportes/:reportId/like', async (req, res) => {
+    const reportId = req.params.reportId;
+    const userId = req.body.userId; // ID del usuario que da like
+  
+    try {
+      const report = await Report.findById(reportId);
+  
+      if (!report.listaDeUsuariosQueDieronLike.includes(userId)) {
+        // Agregar el ID del usuario a la lista de likes y aumentar el contador
+        report.listaDeUsuariosQueDieronLike.push(userId);
+        report.likes += 1;
+        await report.save();
+        
+        res.status(200).json({ message: "Like agregado correctamente." });
+      } else {
+        // El usuario ya dio like, no hacer nada
+        res.status(200).json({ message: "El usuario ya dio like a este reporte." });
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
 module.exports = router;
